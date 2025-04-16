1d41bd18449ecd29e1790ed3366ddfa564f6a136
Elias <elabd.elias@gmail.com>
2025-04-16 18:00:09 +0200

    Add Cpu program counter initialization to the expected memory start address

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,3 +1,9 @@
 #include "cpu.hpp"
 
-Cpu::Cpu(){};
+Cpu::Cpu()
+{
+    // Initialize the programe counter
+    setPC(Chip8Specs::ProgramStartAddress);
+}
+
+void Cpu::setPC(uint16_t value) { pc = value; }
