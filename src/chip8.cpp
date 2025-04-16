325616b3f1f216ec6c152751fdb0b417c437ef4a
Elias <elabd.elias@gmail.com>
2025-04-16 18:18:38 +0200

    Add Chip8 memory fontset loading in class constructor

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,3 +1,10 @@
 #include "chip8.hpp"
 
-Chip8::Chip8(){};
+Chip8::Chip8()
+{
+    // Load all fonts into memory
+    for(uint i {} ; i < Chip8Specs::FontsetSize; ++i)
+    {
+        memory[Chip8Specs::FontSetStartAddress + i] = Chip8Specs::FontSet[i];
+    }
+}
