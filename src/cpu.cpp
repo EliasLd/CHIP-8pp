ec7742fde75d2f7799883c1baf5aa01dcfe504ce
Elias <elabd.elias@gmail.com>
2025-04-17 12:38:22 +0200

    Add 8xy0 instruction (LD vx, vy) -> Set vx = vy

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,5 +1,6 @@
 #include "cpu.hpp"
 #include "chip8.hpp"
+#include "masks.hpp"
 
 Cpu::Cpu()
 {
@@ -9,3 +10,17 @@ Cpu::Cpu()
 
 void Cpu::setSystem(Chip8* sys) { system = sys; }
 void Cpu::setPC(uint16_t value) { pc = value; }
+
+// === Instructions ===
+
+// Arithmetic and logical instructions
+// between registers x and y
+
+// Set registers: vx = vy
+void Cpu::opc_8xy0() 
+{
+    uint8_t vx = (opcode & MASK_OPC_VX) >> 8u;
+    uint8_t vy = (opcode & MASK_OPC_VY) >> 4u;
+
+    registers[vx] = registers[vy];
+}
