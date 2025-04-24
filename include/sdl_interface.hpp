f6cbebf9ad9c275577d00c9e412a505556c12c59
EliasLd <elabd.elias@gmail.com>
2025-04-24 17:44:19 +0200

    Add Chip8 system attribute to SdlInterface class + input processing function

--- include/sdl_interface.hpp
+++ include/sdl_interface.hpp
@@ -2,6 +2,7 @@
 #define CHIP8_SDL_INTERFACE
 
 #include <SDL.h>
+#include "chip8.hpp"
 
 class SdlInterface
 {
@@ -9,12 +10,16 @@ private:
     SDL_Window* window      {nullptr};
     SDL_Renderer* renderer  {nullptr};
     SDL_Texture* texture    {nullptr};
+    Chip8* system           {nullptr};
+
 
 public:
     SdlInterface(const char* window_title,
                 int window_width, int window_height,
-                int texture_width, int texture_height);
+                int texture_width, int texture_height, Chip8* system);
     ~SdlInterface();
+
+    bool HandleKeyInput();
 };
 
 #endif
--- src/sdl_interface.cpp
+++ src/sdl_interface.cpp
@@ -1,8 +1,9 @@
-#include "sdl_interface.hpp";
+#include "sdl_interface.hpp"
+#include "constants.hpp"
 
 SdlInterface::SdlInterface(const char* window_title,
     int window_width, int window_height,
-    int texture_width, int texture_height)
+    int texture_width, int texture_height, Chip8* system) : system {system}
 {
     SDL_Init(SDL_INIT_VIDEO);
 
@@ -20,4 +21,35 @@ SdlInterface::~SdlInterface()
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+bool SdlInterface::HandleKeyInput()
+{
+    bool quit {false};
+
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event))
+    {
+        switch(event.type)
+        {
+        case SDL_QUIT:
+            quit = true;
+            break;
+        case SDL_KEYUP:
+        case SDL_KEYDOWN:
+            // Get the pressed key
+            SDL_Keycode pressed_key { event.key.keysym.sym };
+
+            if(Chip8Specs::KeyMap.count(pressed_key) > 0)
+            {
+                uint8_t chip8_key { Chip8Specs::KeyMap.at(pressed_key) };
+                system->setKeypad(chip8_key, (event.type == SDL_KEYUP ? 0 : 1));
+            }
+
+            break;
+        }
+    }
+
+    return quit;
 }
